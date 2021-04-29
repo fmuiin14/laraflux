132c218d532685a311ae7d678150cc727c8186ba
Fathul Muiin <fmuiin14@gmail.com>
2021-04-29 16:17:39 +0700

    1 41 laravel

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,10 @@
+<h1>Create Product</h1>
+
+<form action="" method="POST">
+    @csrf
+    Name: <input type="text" name="name"><br>
+    Description: <input type="text" name="description"><br>
+    Price: <input type="number" name="price"><br>
+
+    <input type="submit" value="save">
+</form>
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,22 @@
+<h1>List Product</h1>
+
+<table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($products as $product)    
+        <tr>
+            <td>{{$product->name}}</td>
+            <td>{{$product->description}}</td>
+            <td>{{$product->price}}</td>
+        </tr>
+        @endforeach
+    </tbody>
+
+    <a href="/products/create">Tambah Data</a>
+</table>
